bdd6504967accaa8b7dde4a5109bee5e0d19607f
WhoDD <alexwhodd@gmail.com>
2024-02-09 05:39:56 +0500

    update queue

--- src/main.rs
+++ src/main.rs
@@ -15,96 +15,86 @@ struct Player {
     queue_time: u32,
 }
 
-fn create_groups(mut players: Vec<Player>) -> Vec<Vec<Player>> {
-    let mut groups: Vec<Vec<Player>> = Vec::new();
-    let mut current_group: Vec<Player> = Vec::new();
+trait GroupCreator {
+    fn create_groups(&mut self) -> Vec<Vec<Player>>;
+}
 
-    players.sort_by_key(|player| player.queue_time);
-
-    while let Some(player) = players.pop() {
-        if player.queue_time > 5 {
-            current_group.push(player);
-            if current_group.len() == 4 {
-                groups.push(current_group);
-                current_group = Vec::new();
-            }
-        } else {
+impl GroupCreator for Vec<Player> {
+    fn create_groups(&mut self) -> Vec<Vec<Player>> {
+        let mut groups: Vec<Vec<Player>> = Vec::new();
+        let mut tanks: Vec<Player> = Vec::new();
+        let mut healers: Vec<Player> = Vec::new();
+        let mut damagers: Vec<Player> = Vec::new();
+        let mut others: Vec<Player> = Vec::new();
+    
+        self.sort_by_key(|player| player.queue_time);
+    
+        for player in self.iter() {
             match player.role {
-                Role::Tank | Role::Healer | Role::Damager => {
-                    current_group.push(player);
-                }
-                Role::Others => {
-                    current_group.push(player);
-                }
-            }
-            if current_group.len() == 4 {
-                groups.push(current_group);
-                current_group = Vec::new();
+                Role::Tank => tanks.push(player.clone()),
+                Role::Healer => healers.push(player.clone()),
+                Role::Damager => damagers.push(player.clone()),
+                Role::Others => others.push(player.clone()),
             }
         }
+    
+        let mut current_group: Vec<Player> = Vec::new();
+    
+        while !tanks.is_empty() || !healers.is_empty() || !damagers.is_empty() || !others.is_empty() {
+            if current_group.len() == 4 || (!current_group.is_empty() && current_group.last().unwrap().queue_time > 5) {
+                groups.push(current_group.clone());
+                current_group.clear();
+            }
+    
+            if let Some(player) = tanks.pop() {
+                current_group.push(player);
+            }
+            if let Some(player) = healers.pop() {
+                current_group.push(player);
+            }
+            if let Some(player) = damagers.pop() {
+                current_group.push(player);
+            }
+            if let Some(player) = others.pop() {
+                current_group.push(player);
+            }
+        }
+    
+        if !current_group.is_empty() {
+            groups.push(current_group);
+        }
+    
+        groups
     }
     
-    if !current_group.is_empty() {
-        groups.push(current_group);
-    }
-
-    groups
 }
 
 fn main() {
-    
-    let players = vec![
-        Player {
-            id: 1,
-            role: Role::Tank,
-            queue_time: 3,
-        },
-        Player {
-            id: 2,
-            role: Role::Damager,
-            queue_time: 8,
-        },
-        Player {
-            id: 3,
-            role: Role::Healer,
-            queue_time: 2,
-        },
-        Player {
-            id: 4,
-            role: Role::Others,
-            queue_time: 7,
-        },
-        Player {
-            id: 5,
-            role: Role::Damager,
-            queue_time: 1,
-        },
-        Player {
-            id: 6,
-            role: Role::Tank,
-            queue_time: 10,
-        },
-        Player {
-            id: 7,
-            role: Role::Healer,
-            queue_time: 6,
-        },
+    let mut players = vec![
+        Player { id: 1, role: Role::Tank, queue_time: 3 },
+            Player { id: 2, role: Role::Damager, queue_time: 8 },
+            Player { id: 3, role: Role::Healer, queue_time: 2 },
+            Player { id: 4, role: Role::Others, queue_time: 7 },
+            Player { id: 5, role: Role::Damager, queue_time: 1 },
+            Player { id: 6, role: Role::Tank, queue_time: 10 },
+            Player { id: 7, role: Role::Healer, queue_time: 6 },
+            Player { id: 8, role: Role::Others, queue_time: 3 },
     ];
 
-    let groups = create_groups(players);
+    let groups = players.create_groups();
 
     for (i, group) in groups.iter().enumerate() {
         println!("Group {}: {:?}", i + 1, group);
     }
-    
+
     delay();
 }
-fn delay(){
+
+fn delay() {
     let mut close = String::new();
     println!("Нажмите Enter для закрытия программы");
     io::stdin().read_line(&mut close).unwrap();
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -112,33 +102,33 @@ mod tests {
 
     #[test]
     fn test_create_groups() {
-        let players = vec![
-            Player { id: 1, queue_time: 2, role: Role::Tank },
-            Player { id: 2, queue_time: 6, role: Role::Healer },
-            Player { id: 3, queue_time: 4, role: Role::Others },
-            Player { id: 4, queue_time: 1, role: Role::Damager },
-            Player { id: 5, queue_time: 7, role: Role::Others },
-            Player { id: 6, queue_time: 3, role: Role::Tank },
-            Player { id: 7, queue_time: 8, role: Role::Damager },
-            Player { id: 8, queue_time: 9, role: Role::Healer },
+        let mut players = vec![
+            Player { id: 1, role: Role::Tank, queue_time: 3 },
+            Player { id: 2, role: Role::Damager, queue_time: 8 },
+            Player { id: 3, role: Role::Healer, queue_time: 2 },
+            Player { id: 4, role: Role::Others, queue_time: 7 },
+            Player { id: 5, role: Role::Damager, queue_time: 1 },
+            Player { id: 6, role: Role::Tank, queue_time: 10 },
+            Player { id: 7, role: Role::Healer, queue_time: 6 },
         ];
 
-        let expected_groups = vec![
-            vec![Player { id: 8, queue_time: 9, role: Role::Healer }, Player { id: 7, queue_time: 8, role: Role::Damager }, Player { id: 5,  queue_time: 7, role: Role::Others }, Player { id: 2, queue_time: 6, role: Role::Healer }],
-            vec![Player { id: 3, queue_time: 4, role: Role::Others }, Player { id: 6, queue_time: 3, role: Role::Tank }, Player { id: 1, queue_time: 2, role: Role::Tank }, Player { id: 4, queue_time: 1, role: Role::Damager }],
-        ];
+        let groups = players.create_groups();
 
-        let cloned_players: Vec<Player> = players.iter().cloned().collect();
+        for group in &groups {
+            let mut has_tanks = false;
+            let mut has_healers = false;
+            let mut has_damagers = false;
 
-        let result = create_groups(cloned_players);
-        assert_eq!(result.len(), expected_groups.len());
-
-        for (result_group, expected_group) in result.iter().zip(expected_groups.iter()) {
-            assert_eq!(result_group.len(), expected_group.len());
-
-            for (result_player, expected_player) in result_group.iter().zip(expected_group.iter()) {
-                assert_eq!(result_player, expected_player);
+            for player in group {
+                match player.role {
+                    Role::Tank => has_tanks = true,
+                    Role::Healer => has_healers = true,
+                    Role::Damager => has_damagers = true,
+                    _ => (),
+                }
             }
+
+            assert!(has_tanks && has_healers && has_damagers);
         }
     }
 }
